12bd5f36a107f2122681236c4d0f42edf5dabbae
bergi <bergi@axolotlfarm.org>
2016-10-18 21:25:58 +0200

    simplified config file by moving the default stuff to a separate file

--- config.sparql.js
+++ config.sparql.js
@@ -1,98 +1,13 @@
-/* global rdf:false */
+var baseConfig = require('./config.fuseki');
+var defaultsDeep = require('lodash/defaultsDeep');
 
-'use strict';
-
-var
-  fs = require('fs'),
-  path = require('path');
-
-
-var buildQuery = function (iri) {
-  return 'DESCRIBE <' + iri + '>';
-};
-
-var buildExistsQuery = function (iri) {
-  return 'ASK { GRAPH ?g { <' + iri + '> ?p ?o }}';
-};
-
-var patchResponseHeaders = function (res, headers) {
-  if (res.statusCode === 200) {
-    // clean existings values
-    var fieldList = [
-      'Access-Control-Allow-Origin',
-      'Cache-Control',
-      'Fuseki-Request-ID',
-      'Server',
-      'Vary'];
-
-    if (res._headers) {
-      fieldList.forEach(function (field) {
-        if (field in res._headers) {
-          delete res._headers[field];
-        }
-
-        if (field.toLowerCase() in res._headers) {
-          delete res._headers[field.toLowerCase()];
-        }
-      });
-    }
-
-    // cors header
-    headers['Access-Control-Allow-Origin'] = '*';
-
-    // cache header
-    headers['Cache-Control'] = 'public, max-age=120';
-
-    // vary header
-    headers['Vary'] = 'Accept';
-  }
-
-  return headers;
-};
-
-module.exports = {
-  app: 'trifid-ld',
-  logger: {
-    level: 'debug'
-  },
+var config = {
   listener: {
     port: 8080
   },
-  expressSettings: {
-    'trust proxy': 'loopback',
-    'x-powered-by': null
-  },
-  patchHeaders: {
-    patchResponse: patchResponseHeaders
-  },
-  sparqlProxy: {
-    path: '/sparql',
-    options: {
-      /*authentication: {
-        user: 'user',
-        password: 'password'
-      },*/
-      endpointUrl:'http://localhost:3030/tbbt/sparql'
-    }
-  },
-  sparqlSearch: {
-    path: '/query',
-    options: {
-      endpointUrl:'http://localhost:3030/tbbt/sparql',
-      resultsPerPage: 5,
-      queryTemplate: fs.readFileSync(path.join(__dirname, 'data/sparql/search.sparql')).toString(),
-      variables: {
-        'q': {
-          variable: '%searchstring%',
-          required: true
-        }
-      }
-    }
-  },
-  HandlerClass: require('./lib/sparql-handler'),
   handlerOptions: {
-    endpointUrl: 'http://localhost:3030/tbbt/sparql',
-    buildQuery: buildQuery,
-    buildExistsQuery: buildExistsQuery
+    endpointUrl: 'http://localhost:3030/tbbt/sparql'
   }
 };
+
+module.exports = defaultsDeep(config, baseConfig);
