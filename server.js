4a1d05092b4bca0a3686544211d717bf74989e8f
bergi <bergi@axolotlfarm.org>
2016-10-23 16:52:54 +0200

    removed logging of config file path

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var path = require('path')
 var config
 
 try {
-  console.log(path.join(process.cwd(), 'config'))
   config = require(path.join(process.cwd(), 'config'))
 } catch (err) {
   config = require('./config')
