219245a10a2300467209392a45eb6c01c7693f9a
bergi <bergi@axolotlfarm.org>
2016-12-11 23:16:57 +0100

    initial commit

---
+++ packages/yasgui/index.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var express = require('express')
+var fs = require('fs')
+var mustache = require('mustache')
+var path = require('path')
+var url = require('url')
+
+function factory (options) {
+  var router = express.Router()
+
+  if (!options || !options.endpointUrl) {
+    return router
+  }
+
+  var template = fs.readFileSync(options.template).toString()
+
+  // render index page
+  router.get('/', function (req, res) {
+    // read SPARQL endpoint URL from options and resolve with absoluteUrl
+    var locals = {
+      endpointUrl: url.resolve(req.absoluteUrl(), options.endpointUrl)
+    }
+
+    res.setHeader('content-type', 'text/html')
+    res.end(mustache.render(template, locals))
+  })
+
+  // static files from yasgui dist folder
+  router.use('/dist/', express.static(path.resolve(require.resolve('yasgui'), '../../dist/')))
+
+  return router
+}
+
+module.exports = factory
