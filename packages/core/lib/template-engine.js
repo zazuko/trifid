d66e825a59938da3079bed4933dcc1afd9bdc314
bergi <bergi@axolotlfarm.org>
2017-07-14 16:56:23 +0200

    added support for staticViews

--- packages/core/lib/template-engine.js
+++ packages/core/lib/template-engine.js
@@ -2,6 +2,10 @@ var configTools = require('./config')
 var cottonCandy = require('cotton-candy')
 var cottonCandyInclude = require('cotton-candy/include')
 
+/**
+ * Set cotton candy as default template engine with custom settings
+ * @param app
+ */
 function init (app) {
   var resolver = configTools.resolveFilePath.bind(null, app.locals.config)
 
@@ -12,6 +16,25 @@ function init (app) {
     resolve: resolver
   }))
   app.set('view engine', 'cotton-candy')
+}
+
+/**
+ * Adds routes for rendering static templates
+ * @param router
+ * @param options
+ */
+init.staticViews = function (router, options) {
+  if (!options) {
+    return
+  }
+
+  Object.keys(options).forEach(function (urlPath) {
+    var filePath = options[urlPath]
+
+    router.get(urlPath, function (req, res) {
+      res.render(filePath)
+    })
+  })
 }
 
 module.exports = init
