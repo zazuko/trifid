0958d27c59310e82f7cdee9e200d80f70b9cfa43
bergi <bergi@axolotlfarm.org>
2018-01-07 22:05:36 +0100

    removed unnecessary path check, done by mount

--- packages/core/plugins/redirects.js
+++ packages/core/plugins/redirects.js
@@ -1,10 +1,6 @@
 function redirect (router, options) {
   return this.middleware.mountAll(router, options, (config) => {
-    return (req, res, next) => {
-      if (req.originalUrl !== config.path) {
-        return next()
-      }
-
+    return (req, res) => {
       res.redirect(config.target)
     }
   })
