105ed466112c44f5b5d16e4df13c5d817ad4b594
bergi <bergi@axolotlfarm.org>
2018-01-08 14:42:51 +0100

    refactoring based on trifid-core

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
-var configTools = require('./lib/config')
-var path = require('path')
-var program = require('commander')
+const path = require('path')
+const program = require('commander')
+const ConfigHandler = require('trifid-core/lib/ConfigHandler')
+const Trifid = require('trifid-core')
 
 program
+  .option('-v, --verbose', 'verbose output', () => true)
   .option('-c, --config <path>', 'configuration file', 'config.json')
   .option('-p, --port <port>', 'listener port', parseInt)
   .option('--sparql-endpoint-url <url>', 'URL of the SPARQL HTTP query interface')
@@ -17,7 +19,7 @@ if (program.sparqlEndpointUrl && program.config === 'config.json') {
 }
 
 // create a minimal configuration with a baseConfig pointing to the given config file
-var config = {
+const config = {
   baseConfig: path.join(process.cwd(), program.config)
 }
 
@@ -38,8 +40,18 @@ if (program.datasetBaseUrl) {
 }
 
 // load the configuration and start the server
-configTools.fromJson(config).then(function (config) {
-  require('.')(config)
-}).catch(function (err) {
+
+const trifid = new Trifid()
+
+trifid.configHandler.resolver.use('trifid', ConfigHandler.pathResolver(__dirname))
+
+trifid.init(config).then(() => {
+  if (program.verbose) {
+    console.log('expanded config:')
+    console.log(JSON.stringify(trifid.config, null, ' '))
+  }
+
+  return trifid.app()
+}).catch((err) => {
   console.error(err.stack || err.message)
 })
