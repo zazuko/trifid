ee10ac397205f869ccc59b4dd6b9c7e0fa0b7f27
Ludovic Muller <ludovicmuller1@gmail.com>
2022-05-09 16:57:54 +0200

    config: add server field

--- packages/core/lib/config/schema.js
+++ packages/core/lib/config/schema.js
@@ -2,6 +2,28 @@
  * This is the JSON schema representation of the configuration file.
  */
 
+const server = {
+  type: 'object',
+  properties: {
+    listener: {
+      type: 'object',
+      properties: {
+        port: {
+          type: 'number',
+          minimum: 0,
+          maximum: 65535
+        }
+      },
+      additionalProperties: false
+    },
+    express: {
+      type: 'object',
+      additionalProperties: true
+    }
+  },
+  additionalProperties: false
+}
+
 const schema = {
   type: 'object',
   properties: {
@@ -10,7 +32,8 @@ const schema = {
       items: {
         type: 'string'
       }
-    }
+    },
+    server
   },
   additionalProperties: false
 }
