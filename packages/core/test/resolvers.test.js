63f924dc006b1e315b4cc0d00a2e7169b0a8c3b4
Ludovic Muller <ludovicmuller1@gmail.com>
2022-05-11 09:54:48 +0200

    test/resolvers: add envResolver

--- packages/core/test/resolvers.test.js
+++ packages/core/test/resolvers.test.js
@@ -1,17 +1,31 @@
-import { describe, it, expect } from '@jest/globals'
+import { describe, test, expect } from '@jest/globals'
 
-import { envCallback } from '../lib/resolvers.js'
+import { envCallback, envResolver } from '../lib/resolvers.js'
 
 describe('resolvers', () => {
-  it('should be able to resolve an environment variable', () => {
-    delete process.env.TEST_VARIABLE
+  test('should be able to resolve an environment variable', () => {
     process.env.TEST_VARIABLE = 'test'
     expect(envCallback('TEST_VARIABLE')).toEqual('test')
     delete process.env.TEST_VARIABLE
   })
 
-  it('should return an empty string on non-existant environment variables', () => {
+  test('should return an empty string on non-existant environment variables', () => {
     delete process.env.TEST_VARIABLE
     expect(envCallback('TEST_VARIABLE')).toEqual('')
+  })
+
+  test('env should not resolve to anything if it is another prefix', () => {
+    expect(envResolver('something:TEST_VARIABLE')).toEqual('something:TEST_VARIABLE')
+  })
+
+  test('env should resolve with the right prefix', () => {
+    process.env.TEST_VARIABLE = 'test'
+    expect(envResolver('env:TEST_VARIABLE')).toEqual('test')
+    delete process.env.TEST_VARIABLE
+  })
+
+  test('env should resolve to empty string for non-existant variable with the right prefix', () => {
+    delete process.env.TEST_VARIABLE
+    expect(envResolver('env:TEST_VARIABLE')).toEqual('')
   })
 })
