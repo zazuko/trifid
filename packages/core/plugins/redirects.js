1fe7ec17dabb1af7d18577fbf456349855937961
Ludovic Muller <ludovicmuller1@gmail.com>
2022-05-16 14:08:14 +0200

    make it clear what are old plugins

--- packages/core/plugins/redirects.js
+++
@@ -1,9 +0,0 @@
-function redirect (router, options) {
-  return this.middleware.mountAll(router, options, config => {
-    return (req, res) => {
-      res.redirect(config.target)
-    }
-  })
-}
-
-export default redirect
--- packages/core/plugins/static-files.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-function staticFiles (router, options) {
-  return this.middleware.mountAll(router, options, config => {
-    return express.static(config.folder)
-  })
-}
-
-export default staticFiles
