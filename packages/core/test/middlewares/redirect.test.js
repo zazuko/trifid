a3cdaaa70077f7cd70f9cf9b27537d6c4a659f21
Ludovic Muller <ludovicmuller1@gmail.com>
2022-05-16 16:22:41 +0200

    test: redirect middleware

---
+++ packages/core/test/middlewares/redirect.test.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import request from 'supertest'
+import { describe, expect, test } from '@jest/globals'
+
+import redirectMiddleware from '../../middlewares/redirect.js'
+
+describe('redirect middleware', () => {
+  test('should be a function', () => {
+    expect(typeof redirectMiddleware).toEqual('function')
+  })
+
+  test('should throw if the target parameter is not set', async () => {
+    await expect(() => redirectMiddleware({ config: {} })).toThrow()
+  })
+
+  test('should redirect request', async () => {
+    const app = express()
+
+    app.use('/redirect', redirectMiddleware({
+      config: {
+        target: '/'
+      }
+    }))
+
+    return request(app)
+      .get('/redirect')
+      .expect(302)
+  })
+
+  test('should not redirect request', async () => {
+    const app = express()
+
+    app.use('/redirect', redirectMiddleware({
+      config: {
+        target: '/'
+      }
+    }))
+
+    return request(app)
+      .get('/non-existant-route')
+      .expect(404)
+  })
+})
