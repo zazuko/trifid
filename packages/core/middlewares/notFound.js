6babdc0d40bded88b1fef23e783a90a4fd2e9f3c
Ludovic Muller <ludovicmuller1@gmail.com>
2022-05-17 14:44:25 +0200

    views: create 404 view

--- packages/core/middlewares/notFound.js
+++ packages/core/middlewares/notFound.js
@@ -3,7 +3,21 @@ const factory = (trifid) => {
 
   return (req, res, _next) => {
     logger.debug(`path '${req.url}' returned a 404 error (Not Found)`)
-    res.status(404).send('Page not found')
+
+    res.status(404)
+
+    const accepts = req.accepts(['json', 'html'])
+    switch (accepts) {
+      case 'json':
+        res.send({ error: 'Not found' })
+        break
+      case 'html':
+        res.render('404', { url: req.url })
+        break
+      default:
+        res.send('Not Found\n')
+        break
+    }
   }
 }
 
