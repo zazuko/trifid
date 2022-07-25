617b55458aca98409546432acff20b9caa04761a
Cristian <cristianvasquez@gmail.com>
2022-07-25 17:12:11 +0200

    feat: add trifid plugin

---
+++ packages/entity-renderer/index.js
@@ -0,0 +1,66 @@
+import { render as renderWebComponent } from '@lit-labs/ssr/lib/render-with-global-dom-shim.js'
+import { Parser } from 'n3'
+import rdf from 'rdf-ext'
+
+import { ResourceDescription } from './lib/web-component/ResourceDescription.js'
+
+const parser = new Parser()
+
+function toQuads (str) {
+  return parser.parse(str)
+}
+
+function getOptions ({ preferredLanguages, compactMode, highLightLanguage }) {
+  return {
+    embedNamed: false, embedBlanks: false, embedLists: true, groupValuesByProperty: compactMode, groupPropertiesByValue: compactMode, preferredLanguages, highLightLanguage
+  }
+}
+
+class TrifidEntityRenderer {
+  constructor (options) {
+    this.template = options.template
+    this.templateError = options.templateError
+  }
+
+  render (req, res) {
+    this.renderTemplate(this.template, req, res)
+  }
+
+  error (req, res) {
+    res.locals.statusCode = res.statusCode
+    if (this.templateError) {
+      res.render(this.templateError)
+    } else {
+      console.log('Error template needs to be specified')
+    }
+  }
+
+  async _render (template, req, res) {
+    res.render(this.template)
+  }
+
+  renderTemplate (template, req, res) {
+
+    const iri = req.iri
+
+    res.locals.statusCode = res.statusCode
+
+    const cf = rdf.clownface({ dataset: rdf.dataset().addAll(toQuads(res.locals.graph)), term: rdf.namedNode(iri) })
+
+    const defaultLang = ['de', 'fr', 'it', 'en']
+    const selectedLanguage = req.query.lang ? req.query.lang : 'de'
+    const preferredLanguages = selectedLanguage ? [selectedLanguage, ...defaultLang] : defaultLang
+
+    const rendererOptions = getOptions({ preferredLanguages, compactMode: true, highLightLanguage: selectedLanguage })
+
+    const resourceWebComponent = ResourceDescription(cf, rendererOptions)
+    const stringIterator = renderWebComponent(resourceWebComponent)
+
+    res.locals.resourceDescription = Array.from(stringIterator).join('\n')
+
+    this._render(this.template, req, res)
+  }
+
+}
+
+export default TrifidEntityRenderer
