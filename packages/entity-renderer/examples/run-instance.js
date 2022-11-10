9a65922cefa809f3c2acdaa36c3f3c2f8c0267ab
Cristian <cristianvasquez@gmail.com>
2022-11-10 15:01:14 +0100

    chore: simplify examples

--- packages/entity-renderer/examples/run-instance.js
+++ packages/entity-renderer/examples/run-instance.js
@@ -4,7 +4,7 @@ import { join } from 'path'
 import express from 'express'
 
 import trifid from 'trifid-core'
-async function createTrifidInstance (filePath, port) {
+async function createTrifidInstance (filePath) {
   const configFile = join(process.cwd(), filePath)
   const config = {
     extends: [
@@ -14,23 +14,16 @@ async function createTrifidInstance (filePath, port) {
       listener: {}
     }
   }
-  config.server.listener.port = port
   return await trifid(config)
 }
 
-const people = await createTrifidInstance('examples/config/people.yaml', 8080)
-const cube = await createTrifidInstance('examples/config/view.yaml', 8081)
-const adams = await createTrifidInstance('examples/config/adams.yaml', 8082)
-
-// people.start()
-// cube.start()
-// adams.start()
+const people = await createTrifidInstance('examples/config/people.yaml')
+const adams = await createTrifidInstance('examples/config/adams.yaml')
 
 const app = express() // The main app
 const PORT = 3000
 
 app.use('/person', people.server)
-app.use('/view', cube.server)
 app.use('/', adams.server)
 app.listen(PORT, function (err) {
   if (err) console.log(err)
