8eafa750b3140d4fa571b254c1c9849c1554351b
Cristian <cristianvasquez@gmail.com>
2022-11-10 09:51:29 +0100

    chore: simplify example server

--- packages/entity-renderer/examples/run-instance.js
+++ packages/entity-renderer/examples/run-instance.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 import { join } from 'path'
+import express from 'express'
+
+
+
 
 import trifid from 'trifid-core'
 async function createTrifidInstance (filePath, port) {
@@ -17,10 +21,23 @@ async function createTrifidInstance (filePath, port) {
   return await trifid(config)
 }
 
+
+
 const people = await createTrifidInstance('examples/config/people.yaml', 8080)
-const cube = await createTrifidInstance('examples/config/cube.yaml', 8081)
+const cube = await createTrifidInstance('examples/config/view.yaml', 8081)
 const adams = await createTrifidInstance('examples/config/adams.yaml', 8082)
 
-people.start()
-cube.start()
-adams.start()
+// people.start()
+// cube.start()
+// adams.start()
+
+const app = express();  // The main app
+const PORT = 3000;
+
+app.use('/person', people.server);
+app.use('/view', cube.server);
+app.use('/', adams.server);
+app.listen(PORT, function(err){
+  if (err) console.log(err);
+  console.log("Server listening on PORT", PORT);
+});
