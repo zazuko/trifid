c49ad267119abd815b836d06485a4b14d6d8f174
Ludovic Muller <ludovicmuller1@gmail.com>
2022-11-23 13:48:10 +0100

    chore: reword error for express middleware

--- packages/core/middlewares/express.js
+++ packages/core/middlewares/express.js
@@ -14,7 +14,7 @@ const factory = async (trifid) => {
   const { config } = trifid
   const { module, options } = config
   if (!module) {
-    throw new Error("configuration is missing 'module' field")
+    throw new Error("configuration requires 'module' field, specifying the Express middleware NPM module to load")
   }
 
   const middleware = await loader(module)
