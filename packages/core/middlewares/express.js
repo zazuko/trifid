f3f88fbe8d952d5e26f74b705741b92eca82c26e
Ludovic Muller <ludovicmuller1@gmail.com>
2022-11-23 12:11:02 +0100

    feat: allow user to pass options for express middleware

--- packages/core/middlewares/express.js
+++ packages/core/middlewares/express.js
@@ -5,20 +5,21 @@ import { loader } from '../lib/middlewares/loader.js'
  *
  * Configuration fields:
  *  - module (string, required): the name of the NPM module to load
+ *  - options (any, optional): some options to pass to the Express middleware
  *
  * @param {*} trifid Trifid object containing the configuration, and other utility functions.
  * @returns Express middleware.
  */
 const factory = async (trifid) => {
   const { config } = trifid
-  const { module } = config
+  const { module, options } = config
   if (!module) {
     throw new Error("configuration is missing 'module' field")
   }
 
   const middleware = await loader(module)
 
-  return middleware()
+  return middleware(options)
 }
 
 export default factory
