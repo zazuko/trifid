a743798937fe84ca3d17d29436f2b18eb77fedec
Ludovic Muller <ludovicmuller1@gmail.com>
2022-12-08 00:55:55 +0100

    feat(iri): update iri if datasetBaseUrl is configured

--- packages/core/middlewares/iri.js
+++ packages/core/middlewares/iri.js
@@ -23,13 +23,34 @@ const removeSearchParams = (originalUrl) => {
 }
 
 const factory = (trifid) => {
-  const { logger } = trifid
+  const { config, logger } = trifid
+  const { datasetBaseUrl } = config
+
+  // check if `datasetBaseUrl` is a valid URL if present
+  if (datasetBaseUrl) {
+    try {
+      new URL(datasetBaseUrl) // eslint-disable-line no-new
+    } catch (_e) {
+      throw new Error(`The current value you have for 'datasetBaseUrl' is '${datasetBaseUrl}', which is not a valid URL.`)
+    }
+  }
 
   return (req, _res, next) => {
     absoluteUrl.attach(req)
-    req.iri = decodeURI(removeSearchParams(req.absoluteUrl()))
+    const url = req.absoluteUrl()
+    req.iri = decodeURI(removeSearchParams(url))
+
+    // update `req.iri` if a value for `datasetBaseUrl` is provided
+    if (datasetBaseUrl) {
+      const absoluteBaseUrl = new URL('/', url)
+      const currentBaseUrl = absoluteBaseUrl.toString()
+      req.iri = req.iri.replace(currentBaseUrl, datasetBaseUrl)
+      logger.debug(`value for req.iri: ${req.iri} (rewritten)`)
+      return next()
+    }
+
     logger.debug(`value for req.iri: ${req.iri}`)
-    next()
+    return next()
   }
 }
 
