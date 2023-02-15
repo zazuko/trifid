9b4d0806e5bcd6ccbf05ef146ad3a779d80822ea
Ludovic Muller <ludovicmuller1@gmail.com>
2023-02-15 09:54:02 +0100

    morgan: add an option to disable it

--- middlewares/morgan.js
+++ middlewares/morgan.js
@@ -4,8 +4,9 @@ import morgan from 'morgan'
  * Log requests.
  *
  * Configuration fields:
- *  - format (string): format for the morgan logger (default value: "combined")
+ *  - format (string): format for the morgan logger (default: "combined")
  *                     see: https://github.com/expressjs/morgan#predefined-formats
+ *  - disabled (boolean|string): disable morgan logger (default: false)
  *
  * @param {*} trifid Trifid object containing the configuration, and other utility functions.
  * @returns Express middleware.
@@ -13,6 +14,14 @@ import morgan from 'morgan'
 const factory = (trifid) => {
   const { config } = trifid
   const format = config.format ?? 'combined'
+  const disabled = `${config.disabled}` === 'true'
+
+  if (disabled) {
+    return (_req, _res, next) => {
+      next()
+    }
+  }
+
   return morgan(format)
 }
 
