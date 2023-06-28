389e5d9acfbe05bcbe377d102873530f2d7c4340
Ludovic Muller <ludovicmuller1@gmail.com>
2023-06-28 18:48:51 +0200

    import spex

--- packages/spex/test/test.js
+++ packages/spex/test/test.js
@@ -1,81 +1,81 @@
-import assert from 'assert'
-import { describe, it } from 'mocha'
-import trifidFactory from '../index.js'
-import request from 'supertest'
-import express from 'express'
+import assert from "assert";
+import { describe, it } from "mocha";
+import request from "supertest";
+import express from "express";
+import trifidFactory from "../index.js";
 
 const createTrifidConfig = (config) => {
   return {
     config,
     render: async (_templatePath, _templateOptions, _layoutOptions) => {
-      return 'OK'
-    }
-  }
-}
+      return "OK";
+    },
+  };
+};
 
-describe('trifid-plugin-spex', () => {
-  describe('trifid factory', () => {
-    it('should be a factory', () => {
-      assert.strictEqual(typeof trifidFactory, 'function')
-    })
+describe("trifid-plugin-spex", () => {
+  describe("trifid factory", () => {
+    it("should be a factory", () => {
+      assert.strictEqual(typeof trifidFactory, "function");
+    });
 
-    it('should create a middleware with factory and default options', async () => {
-      const trifid = createTrifidConfig({})
-      const middleware = await trifidFactory(trifid)
+    it("should create a middleware with factory and default options", async () => {
+      const trifid = createTrifidConfig({});
+      const middleware = await trifidFactory(trifid);
 
-      assert.strictEqual(typeof middleware, 'function')
-    })
-  })
+      assert.strictEqual(typeof middleware, "function");
+    });
+  });
 
-  describe('middleware', () => {
-    it('should redirect to a version with a trailing slash', async () => {
+  describe("middleware", () => {
+    it("should redirect to a version with a trailing slash", async () => {
       const trifid = createTrifidConfig({
-        endpointUrl: '/test'
-      })
-      const middleware = await trifidFactory(trifid)
+        endpointUrl: "/test",
+      });
+      const middleware = await trifidFactory(trifid);
 
-      const app = express()
-      app.use('/spex', middleware)
+      const app = express();
+      app.use("/spex", middleware);
 
-      await request(app).get('/spex').expect(302)
-    })
+      await request(app).get("/spex").expect(302);
+    });
 
-    it('can execute', async () => {
+    it("can execute", async () => {
       const trifid = createTrifidConfig({
-        endpointUrl: '/test'
-      })
-      const middleware = await trifidFactory(trifid)
+        endpointUrl: "/test",
+      });
+      const middleware = await trifidFactory(trifid);
 
-      const app = express()
-      app.use('/spex', middleware)
+      const app = express();
+      app.use("/spex", middleware);
 
-      await request(app).get('/spex/').expect(200)
-    })
-  })
+      await request(app).get("/spex/").expect(200);
+    });
+  });
 
-  describe('static assets', () => {
-    it('should be able to provide the static JavaScript file', async () => {
+  describe("static assets", () => {
+    it("should be able to provide the static JavaScript file", async () => {
       const trifid = createTrifidConfig({
-        endpointUrl: '/test'
-      })
-      const middleware = await trifidFactory(trifid)
+        endpointUrl: "/test",
+      });
+      const middleware = await trifidFactory(trifid);
 
-      const app = express()
-      app.use('/spex', middleware)
+      const app = express();
+      app.use("/spex", middleware);
 
-      await request(app).get('/spex/static/spex.umd.min.js').expect(200)
-    })
+      await request(app).get("/spex/static/spex.umd.min.js").expect(200);
+    });
 
-    it('should be able to provide the static CSS file', async () => {
+    it("should be able to provide the static CSS file", async () => {
       const trifid = createTrifidConfig({
-        endpointUrl: '/test'
-      })
-      const middleware = await trifidFactory(trifid)
+        endpointUrl: "/test",
+      });
+      const middleware = await trifidFactory(trifid);
 
-      const app = express()
-      app.use('/spex', middleware)
+      const app = express();
+      app.use("/spex", middleware);
 
-      await request(app).get('/spex/static/spex.css').expect(200)
-    })
-  })
-})
+      await request(app).get("/spex/static/spex.css").expect(200);
+    });
+  });
+});
