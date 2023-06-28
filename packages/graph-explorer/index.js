b210a4665f135240f8c3c2e13dbe5e72d74623b7
Ludovic Muller <ludovicmuller1@gmail.com>
2023-06-28 18:25:10 +0200

    import graph-explorer

--- packages/graph-explorer/index.js
+++ packages/graph-explorer/index.js
@@ -1,13 +1,13 @@
-import absoluteUrl from 'absolute-url'
-import { resolve } from 'import-meta-resolve'
-import express from 'express'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import absoluteUrl from "absolute-url";
+import { resolve } from "import-meta-resolve";
+import express from "express";
 
-const currentDir = dirname(fileURLToPath(import.meta.url))
+const currentDir = dirname(fileURLToPath(import.meta.url));
 
 const factory = async (trifid) => {
-  const { config, server, render } = trifid
+  const { config, server, render } = trifid;
   const {
     template,
     endpointUrl,
@@ -15,58 +15,68 @@ const factory = async (trifid) => {
     dataLabelProperty: dataLabelPropertyConfig,
     schemaLabelProperty: schemaLabelPropertyConfig,
     language: languageConfig,
-    languages: languagesConfig
-  } = config
+    languages: languagesConfig,
+  } = config;
 
-  const view = !template ? `${currentDir}/views/graph-explorer.hbs` : template
+  const view = !template ? `${currentDir}/views/graph-explorer.hbs` : template;
 
   // serve static files for graph-explorer
-  const distPath = await resolve('graph-explorer/dist/', import.meta.url)
-  server.use('/graph-explorer-assets/', express.static(distPath.replace(/^file:\/\//, '')))
-  server.use('/graph-explorer-static/', express.static(`${currentDir}/static/`))
+  const distPath = await resolve("graph-explorer/dist/", import.meta.url);
+  server.use(
+    "/graph-explorer-assets/",
+    express.static(distPath.replace(/^file:\/\//, ""))
+  );
+  server.use(
+    "/graph-explorer-static/",
+    express.static(`${currentDir}/static/`)
+  );
 
-  const endpoint = endpointUrl || '/query'
-  const acceptBlankNodes = !!acceptBlankNodesConfig
-  const dataLabelProperty = dataLabelPropertyConfig || 'rdfs:label'
-  const schemaLabelProperty = schemaLabelPropertyConfig || 'rdfs:label'
-  const language = languageConfig || 'en'
+  const endpoint = endpointUrl || "/query";
+  const acceptBlankNodes = !!acceptBlankNodesConfig;
+  const dataLabelProperty = dataLabelPropertyConfig || "rdfs:label";
+  const schemaLabelProperty = schemaLabelPropertyConfig || "rdfs:label";
+  const language = languageConfig || "en";
   const languages = languagesConfig || [
-    { code: 'en', label: 'English' },
-    { code: 'de', label: 'German' },
-    { code: 'fr', label: 'French' },
-    { code: 'it', label: 'Italian' }
-  ]
+    { code: "en", label: "English" },
+    { code: "de", label: "German" },
+    { code: "fr", label: "French" },
+    { code: "it", label: "Italian" },
+  ];
 
   return async (req, res, _next) => {
-    absoluteUrl.attach(req)
+    absoluteUrl.attach(req);
 
-    const urlPathname = new URL(req.originalUrl, req.absoluteUrl()).pathname
+    const urlPathname = new URL(req.originalUrl, req.absoluteUrl()).pathname;
 
     // redirect to trailing slash URL
-    if (urlPathname.slice(-1) !== '/') {
-      return res.redirect(`${urlPathname}/`)
+    if (urlPathname.slice(-1) !== "/") {
+      return res.redirect(`${urlPathname}/`);
     }
 
-    const content = await render(view, {
-      // just forward all the config as a string
-      graphExplorerConfig: JSON.stringify({
-        // read SPARQL endpoint URL from configuration and resolve with absoluteUrl
-        endpointUrl: new URL(endpoint, req.absoluteUrl()).href,
+    const content = await render(
+      view,
+      {
+        // just forward all the config as a string
+        graphExplorerConfig: JSON.stringify({
+          // read SPARQL endpoint URL from configuration and resolve with absoluteUrl
+          endpointUrl: new URL(endpoint, req.absoluteUrl()).href,
 
-        // all other configured options
-        acceptBlankNodes,
-        dataLabelProperty,
-        schemaLabelProperty,
-        language,
-        languages
-      }).replace(/'/g, "\\'"),
+          // all other configured options
+          acceptBlankNodes,
+          dataLabelProperty,
+          schemaLabelProperty,
+          language,
+          languages,
+        }).replace(/'/g, "\\'"),
 
-      // good practice: forward locals to templates
-      locals: res.locals
-    }, { title: 'Graph Explorer' })
+        // good practice: forward locals to templates
+        locals: res.locals,
+      },
+      { title: "Graph Explorer" }
+    );
 
-    res.send(content)
-  }
-}
+    res.send(content);
+  };
+};
 
-export default factory
+export default factory;
