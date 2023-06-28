7d2e00a906b1415683d612c0313c19c1f65c383b
Ludovic Muller <ludovicmuller1@gmail.com>
2023-06-28 17:43:52 +0200

    import sparql-proxy

--- packages/sparql-proxy/index.js
+++ packages/sparql-proxy/index.js
@@ -1,30 +1,36 @@
-import sparqlProxy from '@zazuko/sparql-proxy'
+import sparqlProxy from "@zazuko/sparql-proxy";
 
 const factory = (trifid) => {
-  const { config } = trifid
+  const { config } = trifid;
 
   const {
     endpointUrl: _e, // ignore this field
     authentication: _a, // ignore this field
     sparqlEndpoint, // get the configuration about the endpoint
     ...proxyConfig // rest of the configuration
-  } = config
+  } = config;
 
   if (sparqlEndpoint) {
-    if (Object.hasOwnProperty.call(sparqlEndpoint, 'url')) {
-      proxyConfig.endpointUrl = sparqlEndpoint.url
+    if (Object.hasOwnProperty.call(sparqlEndpoint, "url")) {
+      proxyConfig.endpointUrl = sparqlEndpoint.url;
     }
 
-    const hasProperties = Object.hasOwnProperty.call(sparqlEndpoint, 'username') && Object.hasOwnProperty.call(sparqlEndpoint, 'password')
-    if (hasProperties && sparqlEndpoint.username !== '' && sparqlEndpoint.password !== '') {
+    const hasProperties =
+      Object.hasOwnProperty.call(sparqlEndpoint, "username") &&
+      Object.hasOwnProperty.call(sparqlEndpoint, "password");
+    if (
+      hasProperties &&
+      sparqlEndpoint.username !== "" &&
+      sparqlEndpoint.password !== ""
+    ) {
       proxyConfig.authentication = {
         user: sparqlEndpoint.username,
-        password: sparqlEndpoint.password
-      }
+        password: sparqlEndpoint.password,
+      };
     }
   }
 
-  return sparqlProxy(proxyConfig)
-}
+  return sparqlProxy(proxyConfig);
+};
 
-export default factory
+export default factory;
