8b43fcd72f6927a433fb7c7728afdc94c9c42596
Ludovic Muller <ludovicmuller1@gmail.com>
2023-06-28 19:02:12 +0200

    import yasgui

--- packages/yasgui/plugins/map.js
+++ packages/yasgui/plugins/map.js
@@ -1,16 +1,16 @@
 /* global Yasr */
 
 class YasguiMap {
-  priority = 10
+  priority = 10;
 
-  hideFromSelection = false
+  hideFromSelection = false;
 
   // eslint-disable-next-line space-before-function-paren
   constructor(yasr) {
-    this.yasr = yasr
+    this.yasr = yasr;
   }
 
-  getResults () {
+  getResults() {
     if (
       !this.yasr ||
       !this.yasr.results ||
@@ -18,75 +18,78 @@ class YasguiMap {
       !this.yasr.results.json.results ||
       !this.yasr.results.json.results.bindings
     ) {
-      return []
+      return [];
     }
 
-    const results = this.yasr.results.json.results.bindings
+    const results = this.yasr.results.json.results.bindings;
 
     if (results.length < 1) {
-      return []
+      return [];
     }
 
-    const wktData = []
+    const wktData = [];
 
     // eslint-disable-next-line array-callback-return
     results.map((result) => {
       if (!result) {
-        return null
+        return null;
       }
 
       // eslint-disable-next-line array-callback-return
       Object.entries(result).map((entry) => {
         if (!entry[1]) {
-          return null
+          return null;
         }
-        const value = entry[1]
-        if (!value.type || value.type !== 'literal') {
-          return null
+        const value = entry[1];
+        if (!value.type || value.type !== "literal") {
+          return null;
         }
-        if (!value.datatype || value.datatype !== 'http://www.opengis.net/ont/geosparql#wktLiteral') {
-          return null
+        if (
+          !value.datatype ||
+          value.datatype !== "http://www.opengis.net/ont/geosparql#wktLiteral"
+        ) {
+          return null;
         }
         if (!value.value) {
-          return null
+          return null;
         }
 
         wktData.push({
           id: `results-map-wkt-${entry[0]}`,
-          wkt: value.value
-        })
-      })
-    })
+          wkt: value.value,
+        });
+      });
+    });
 
-    return wktData
+    return wktData;
   }
 
-  draw () {
-    const results = this.getResults()
-    const el = document.createElement('ol-map')
-    const osm = document.createElement('ol-layer-openstreetmap')
-    const wkt = document.createElement('ol-layer-wkt')
-    osm.appendChild(wkt)
-    el.appendChild(osm)
-    this.yasr.resultsEl.appendChild(el)
+  draw() {
+    const results = this.getResults();
+    const el = document.createElement("ol-map");
+    const osm = document.createElement("ol-layer-openstreetmap");
+    const wkt = document.createElement("ol-layer-wkt");
+    osm.appendChild(wkt);
+    el.appendChild(osm);
+    this.yasr.resultsEl.appendChild(el);
 
-    wkt.featureData = results
+    wkt.featureData = results;
 
     setTimeout(() => {
-      wkt.fit()
-    }, 200)
+      wkt.fit();
+    }, 200);
   }
 
-  canHandleResults () {
-    const results = this.getResults()
-    return results.length > 0
+  canHandleResults() {
+    const results = this.getResults();
+    return results.length > 0;
   }
 
-  getIcon () {
-    const textIcon = document.createElement('div')
-    textIcon.innerText = '🌐'
-    return textIcon
+  getIcon() {
+    const textIcon = document.createElement("div");
+    textIcon.innerText = "🌐";
+    return textIcon;
   }
 }
 
-Yasr.registerPlugin('Map', YasguiMap)
+Yasr.registerPlugin("Map", YasguiMap);
--- packages/yasgui/test/test.js
+++ packages/yasgui/test/test.js
@@ -1,105 +1,109 @@
-import assert from 'assert'
-import request from 'supertest'
-import { describe, it } from 'mocha'
-import trifidFactory from '../index.js'
-import express from 'express'
-import absoluteUrl from 'absolute-url'
+import assert from "assert";
+import request from "supertest";
+import { describe, it } from "mocha";
+import express from "express";
+import absoluteUrl from "absolute-url";
+import trifidFactory from "../index.js";
 
 const createTrifidConfig = (app, config) => {
-  const server = app
-  const logger = console
+  const server = app;
+  const logger = console;
   const render = (filePath, context, options) => {
-    return JSON.stringify({
-      filePath,
-      context,
-      options
-    }, null, 2)
-  }
+    return JSON.stringify(
+      {
+        filePath,
+        context,
+        options,
+      },
+      null,
+      2
+    );
+  };
 
   return {
     config,
     server,
     logger,
-    render
-  }
-}
+    render,
+  };
+};
 
-describe('trifid-plugin-yasgui', () => {
-  describe('trifid factory', () => {
-    it('should be a factory', () => {
-      assert.strictEqual(typeof trifidFactory, 'function')
-    })
+describe("trifid-plugin-yasgui", () => {
+  describe("trifid factory", () => {
+    it("should be a factory", () => {
+      assert.strictEqual(typeof trifidFactory, "function");
+    });
 
-    it('should create a middleware with factory and default options', async () => {
-      const app = express()
-      const trifid = createTrifidConfig(app, {})
-      const middleware = await trifidFactory(trifid)
+    it("should create a middleware with factory and default options", async () => {
+      const app = express();
+      const trifid = createTrifidConfig(app, {});
+      const middleware = await trifidFactory(trifid);
 
-      assert.strictEqual(typeof middleware, 'function')
-    })
-  })
+      assert.strictEqual(typeof middleware, "function");
+    });
+  });
 
-  describe('middleware', () => {
-    it('can execute', (done) => {
-      const app = express()
-      app.use(absoluteUrl())
+  describe("middleware", () => {
+    it("can execute", (done) => {
+      const app = express();
+      app.use(absoluteUrl());
 
-      const trifidConfig = createTrifidConfig(app, {})
-      trifidFactory(trifidConfig).then(middleware => {
-        app.use('/sparql', middleware)
+      const trifidConfig = createTrifidConfig(app, {});
+      trifidFactory(trifidConfig).then((middleware) => {
+        app.use("/sparql", middleware);
         request(app)
-          .get('/sparql')
+          .get("/sparql")
           .expect(200)
           .end((err, _res) => {
             if (err) {
-              done(err)
+              done(err);
             } else {
-              done()
+              done();
             }
-          })
-      })
-    })
-  })
+          });
+      });
+    });
+  });
 
-  describe('YASGUI dist', () => {
-    it('can serve static CSS style', (done) => {
-      const app = express()
-      app.use(absoluteUrl())
+  describe("YASGUI dist", () => {
+    it("can serve static CSS style", (done) => {
+      const app = express();
+      app.use(absoluteUrl());
 
-      const trifidConfig = createTrifidConfig(app, {})
-      trifidFactory(trifidConfig).then(middleware => {
-        app.use('/sparql', middleware)
+      const trifidConfig = createTrifidConfig(app, {});
+      trifidFactory(trifidConfig).then((middleware) => {
+        app.use("/sparql", middleware);
         request(app)
-          .get('/yasgui-dist/yasgui.min.css')
+          .get("/yasgui-dist/yasgui.min.css")
           .expect(200)
           .end((err, _res) => {
             if (err) {
-              done(err)
+              done(err);
             } else {
-              done()
+              done();
             }
-          })
-      })
-    })
+          });
+      });
+    });
 
-    it('can serve static JavaScript script', (done) => {
-      const app = express()
-      app.use(absoluteUrl())
+    it("can serve static JavaScript script", (done) => {
+      const app = express();
+      app.use(absoluteUrl());
 
-      const trifidConfig = createTrifidConfig(app, {})
-      trifidFactory(trifidConfig).then(middleware => {
-        app.use('/sparql', middleware)
+      const trifidConfig = createTrifidConfig(app, {});
+      trifidFactory(trifidConfig).then((middleware) => {
+        app.use("/sparql", middleware);
         request(app)
-          .get('/yasgui-dist/yasgui.min.js')
+          .get("/yasgui-dist/yasgui.min.js")
           .expect(200)
           .end((err, _res) => {
             if (err) {
-              done(err)
+              done(err);
             } else {
-              done()
+              done();
             }
-          })
-      })
-    })
-  })
-})
+          });
+      });
+    });
+  });
+});
