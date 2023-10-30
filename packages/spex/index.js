ddbf21080434d5331201009d16c2fae2df718ebc
Ludovic Muller <ludovicmuller1@gmail.com>
2023-10-30 16:01:19 +0100

    spex: format code

--- packages/spex/index.js
+++ packages/spex/index.js
@@ -1,14 +1,14 @@
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import absoluteUrl from "absolute-url";
-import { resolve } from "import-meta-resolve";
-import express from "express";
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import absoluteUrl from 'absolute-url'
+import { resolve } from 'import-meta-resolve'
+import express from 'express'
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const defaults = {
-  template: path.join(__dirname, "views/index.hbs"),
-};
+  template: path.join(__dirname, 'views/index.hbs'),
+}
 
 const defaultOptions = {
   url: null,
@@ -17,12 +17,12 @@ const defaultOptions = {
   graph: null,
   prefixes: [],
   forceIntrospection: false,
-};
+}
 
 const createMiddleWare = async (config, render) => {
-  const router = express.Router();
+  const router = express.Router()
 
-  const options = { ...defaultOptions, ...(config || {}) };
+  const options = { ...defaultOptions, ...(config || {}) }
   const spexOptions = {
     sparqlEndpoint: options.url,
     username: options.user,
@@ -30,23 +30,23 @@ const createMiddleWare = async (config, render) => {
     forceIntrospection: options.forceIntrospection,
     namedGraph: options.graph,
     prefixes: options.prefixes,
-  };
-  config = { ...defaults, ...config, spexOptions };
+  }
+  config = { ...defaults, ...config, spexOptions }
 
   // render index page
-  router.get("/", async (req, res) => {
+  router.get('/', async (req, res) => {
     // Enforce trailing slash to ensure that static files are served from the correct URL
-    if (!req.originalUrl.endsWith("/")) {
-      return res.redirect(req.originalUrl + "/");
+    if (!req.originalUrl.endsWith('/')) {
+      return res.redirect(req.originalUrl + '/')
     }
 
-    absoluteUrl.attach(req);
+    absoluteUrl.attach(req)
 
     // Create an absolute URL if a relative URL is provided
     spexOptions.sparqlEndpoint = new URL(
-      spexOptions.sparqlEndpoint || "/query",
+      spexOptions.sparqlEndpoint || '/query',
       req.absoluteUrl(),
-    ).toString();
+    ).toString()
 
     res.send(
       await render(
@@ -56,22 +56,22 @@ const createMiddleWare = async (config, render) => {
           locals: res.locals,
         },
         {
-          title: "SPEX",
+          title: 'SPEX',
         },
       ),
-    );
-  });
+    )
+  })
 
   // static files from spex dist folder
-  const distPath = await resolve("@zazuko/spex/dist", import.meta.url);
-  router.use("/static/", express.static(distPath.replace(/^file:\/\//, "")));
-  return router;
-};
+  const distPath = await resolve('@zazuko/spex/dist', import.meta.url)
+  router.use('/static/', express.static(distPath.replace(/^file:\/\//, '')))
+  return router
+}
 
 const trifidFactory = async (trifid) => {
-  const { config, render } = trifid;
-  return await createMiddleWare(config, render);
-};
+  const { config, render } = trifid
+  return await createMiddleWare(config, render)
+}
 
-export default trifidFactory;
-export { createMiddleWare };
+export default trifidFactory
+export { createMiddleWare }
