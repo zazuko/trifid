e873b2f16b48074544b847b1e6efdb2269bef746
Ludovic Muller <ludovicmuller1@gmail.com>
2023-10-31 16:37:09 +0100

    ckan: added tests + blank line at the end

---
+++ .changeset/four-rabbits-invite.md
@@ -0,0 +1,7 @@
+---
+"@zazuko/trifid-plugin-ckan": patch
+---
+
+Generated XML documents have a blank line at the end.
+
+This Trifid plugin will be more robust, as we created a set of tests.
--- packages/ckan/test/ckan.test.js
+++ packages/ckan/test/ckan.test.js
@@ -1,29 +1,88 @@
-import assert from 'assert'
-import withServer from 'express-as-promise/withServer.js'
+// @ts-check
+import { strictEqual } from 'assert'
+import { readFile } from 'fs/promises'
 import { describe, it } from 'mocha'
-import trifidFactory from '../src/index.js'
 
-const createTrifidConfig = (config, server = {}) => {
-  const loggerSpy = []
+import trifidCore from '../../core/index.js'
+import ckanTrifidPlugin from '../src/index.js'
+import { storeMiddleware } from './support/store.js'
+import { getListenerURL } from './support/utils.js'
 
-  return {
-    logger: (str) => loggerSpy.push(str),
-    server,
-    config,
-  }
+const createTrifidInstance = async () => {
+  return await trifidCore({
+    server: {
+      listener: {
+        port: 4242,
+      },
+      logLevel: 'warn',
+    },
+  }, {
+    store: {
+      module: storeMiddleware,
+      paths: ['/query'],
+      methods: ['GET', 'POST'],
+    },
+    ckan: {
+      module: ckanTrifidPlugin,
+      paths: ['/ckan'],
+      methods: ['GET'],
+      config: {
+        endpointUrl: '/query',
+      },
+    },
+  })
 }
 
 describe('@zazuko/trifid-plugin-ckan', () => {
   describe('trifid factory', () => {
     it('should be a factory', () => {
-      assert.strictEqual(typeof trifidFactory, 'function')
+      strictEqual(typeof ckanTrifidPlugin, 'function')
     })
 
     it('should create a middleware with factory and default options', async () => {
-      await withServer(async (server) => {
-        const trifid = createTrifidConfig({}, server.app)
-        trifidFactory(trifid)
-      })
+      const trifidInstance = await createTrifidInstance()
+      const trifidListener = await trifidInstance.start()
+      trifidListener.close()
+    })
+
+    it('should answer with a 400 status code if the organization parameter is missing', async () => {
+      const trifidInstance = await createTrifidInstance()
+      const trifidListener = await trifidInstance.start()
+      const ckanUrl = `${getListenerURL(trifidListener)}/ckan`
+
+      const res = await fetch(ckanUrl)
+      strictEqual(res.status, 400)
+      trifidListener.close()
+    })
+
+    it('should get an empty result for an unknown organization', async () => {
+      const trifidInstance = await createTrifidInstance()
+      const trifidListener = await trifidInstance.start()
+      const ckanUrl = `${getListenerURL(trifidListener)}/ckan?organization=http://example.com/unkown-org`
+
+      const res = await fetch(ckanUrl)
+      const body = await res.text()
+      const expectedResult = await readFile(new URL('./support/empty-result.xml', import.meta.url), 'utf8')
+
+      strictEqual(res.status, 200)
+      strictEqual(body, expectedResult)
+
+      trifidListener.close()
+    })
+
+    it('should get a basic result for a known organization', async () => {
+      const trifidInstance = await createTrifidInstance()
+      const trifidListener = await trifidInstance.start()
+      const ckanUrl = `${getListenerURL(trifidListener)}/ckan?organization=http://example.com/my-org`
+
+      const res = await fetch(ckanUrl)
+      const body = await res.text()
+      const expectedResult = await readFile(new URL('./support/basic-result.xml', import.meta.url), 'utf8')
+
+      strictEqual(res.status, 200)
+      strictEqual(body, expectedResult)
+
+      trifidListener.close()
     })
   })
 })
