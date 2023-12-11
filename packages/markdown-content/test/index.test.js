d6e6c219fbe34c7129ab0e72e8ac1f1fb15ef2a7
Ludovic Muller <ludovicmuller1@gmail.com>
2023-12-11 12:21:50 +0100

    markdown-content: configure tests

---
+++ packages/markdown-content/test/index.test.js
@@ -0,0 +1,9 @@
+// import { strictEqual } from 'assert'
+import { describe, it } from 'mocha'
+
+describe('@zazuko/trifid-markdown-content', () => {
+  describe('basic tests', () => {
+    it('should create a middleware with factory and default options', async () => {
+    })
+  })
+})
