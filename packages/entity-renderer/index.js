81c27c027505e93eb9db30664af141e4c4cf687d
Ludovic Muller <ludovicmuller1@gmail.com>
2024-02-20 16:10:52 +0100

    entity-renderer: use new Trifid factory

--- packages/entity-renderer/index.js
+++ packages/entity-renderer/index.js
@@ -1,8 +1,6 @@
-/* eslint-disable no-template-curly-in-string */
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { parsers } from '@rdfjs/formats-common'
-import absoluteUrl from 'absolute-url'
 
 import rdf from '@zazuko/env'
 import { sparqlSerializeQuadStream, sparqlSupportedTypes, sparqlGetRewriteConfiguration } from 'trifid-core'
@@ -43,7 +41,6 @@ const factory = async (trifid) => {
 
   const { path, ignorePaths, rewrite: rewriteConfigValue, datasetBaseUrl } = config
   const entityTemplatePath = path || `${currentDir}/views/render.hbs`
-
   const rewriteConfig = sparqlGetRewriteConfiguration(rewriteConfigValue, datasetBaseUrl)
   const { rewrite: rewriteValue, replaceIri, iriOrigin } = rewriteConfig
   logger.debug(`Rewriting is ${rewriteValue ? 'enabled' : 'disabled'}`)
@@ -61,89 +58,107 @@ const factory = async (trifid) => {
     ignoredPaths = ['/query']
   }
 
-  return async (req, res, next) => {
-    // Check if it is a path that needs to be ignored (check of type is already done at the load of the middleware)
-    if (ignoredPaths.includes(req.path)) {
-      return next()
-    }
-
-    // Get the expected format from the Accept header or from the `format` query parameter
-    const acceptHeader = getAcceptHeader(req)
-
-    // Generate the IRI we expect
-    const iriUrl = new URL(encodeURI(absoluteUrl(req)))
-    iriUrl.search = ''
-    iriUrl.searchParams.forEach((_value, key) => iriUrl.searchParams.delete(key))
-    const iriUrlString = iriUrl.toString()
-    const iri = replaceIri(iriUrlString)
-    logger.debug(`IRI value: ${iri}${rewriteValue ? ' (rewritten)' : ''}`)
-    const rewriteResponse = rewriteValue
-      ? [
-        { find: datasetBaseUrl, replace: iriOrigin(iriUrlString) },
-      ]
-      : []
-
-    // Check if the IRI exists in the dataset
-    // @TODO: allow the user to configure the query
-    const askQuery = 'ASK { <{{iri}}> ?p ?o }'
-    const exists = await query(replaceIriInQuery(askQuery, iri), { ask: true })
-    if (!exists) {
-      return next()
-    }
-
-    try {
-      // Get the entity from the dataset
-      // @TODO: allow the user to configure the query
-      const describeQuery = 'DESCRIBE <{{iri}}>'
-      const entity = await query(replaceIriInQuery(describeQuery, iri), {
-        ask: false,
-        rewriteResponse,
-      })
-      const entityContentType = entity.contentType || 'application/n-triples'
-      const entityStream = entity.response
-      if (!entityStream) {
-        return next()
+  return {
+    defaultConfiguration: async () => {
+      return {
+        methods: ['GET'],
+        paths: ['/*'],
       }
+    },
+    routeHandler: async () => {
+      /**
+       * Route handler.
+       * @param {import('fastify').FastifyRequest} request Request.
+       * @param {import('fastify').FastifyReply} reply Reply.
+       */
+      const handler = async (request, reply) => {
+        const currentPath = request.url.split('?')[0]
+        // Check if it is a path that needs to be ignored (check of type is already done at the load of the middleware)
+        if (ignoredPaths.includes(currentPath)) {
+          reply.callNotFound()
+          return
+        }
 
-      // Make sure the Content-Type is lower case and without parameters (e.g. charset)
-      const fixedContentType = entityContentType.split(';')[0].trim().toLocaleLowerCase()
+        // Get the expected format from the Accept header or from the `format` query parameter
+        const acceptHeader = getAcceptHeader(request)
 
-      const quadStream = parsers.import(fixedContentType, entityStream)
+        // Generate the IRI we expect
+        const fullUrl = `${request.protocol}://${request.hostname}${request.raw.url}`
+        const iriUrl = new URL(fullUrl)
+        iriUrl.search = ''
+        iriUrl.searchParams.forEach((_value, key) => iriUrl.searchParams.delete(key))
+        const iriUrlString = iriUrl.toString()
+        const iri = replaceIri(iriUrlString)
+        logger.debug(`IRI value: ${iri}${rewriteValue ? ' (rewritten)' : ''}`)
+        const rewriteResponse = rewriteValue
+          ? [
+            { find: datasetBaseUrl, replace: iriOrigin(iriUrlString) },
+          ]
+          : []
 
-      if (sparqlSupportedTypes.includes(acceptHeader)) {
-        const serialized = await sparqlSerializeQuadStream(quadStream, acceptHeader)
-        res.setHeader('Content-Type', acceptHeader)
-        res.send(serialized)
-        return
+        // Check if the IRI exists in the dataset
+        // @TODO: allow the user to configure the query
+        const askQuery = 'ASK { <{{iri}}> ?p ?o }'
+        const exists = await query(replaceIriInQuery(askQuery, iri), { ask: true })
+        if (!exists) {
+          reply.callNotFound()
+          return
+        }
+
+        try {
+          // Get the entity from the dataset
+          // @TODO: allow the user to configure the query
+          const describeQuery = 'DESCRIBE <{{iri}}>'
+          const entity = await query(replaceIriInQuery(describeQuery, iri), {
+            ask: false,
+            rewriteResponse,
+          })
+          const entityContentType = entity.contentType || 'application/n-triples'
+          const entityStream = entity.response
+          if (!entityStream) {
+            reply.callNotFound()
+            return
+          }
+
+          // Make sure the Content-Type is lower case and without parameters (e.g. charset)
+          const fixedContentType = entityContentType.split(';')[0].trim().toLocaleLowerCase()
+
+          const quadStream = parsers.import(fixedContentType, entityStream)
+
+          if (sparqlSupportedTypes.includes(acceptHeader)) {
+            const serialized = await sparqlSerializeQuadStream(quadStream, acceptHeader)
+            reply.type(acceptHeader).send(serialized)
+            return
+          }
+
+          const dataset = await rdf.dataset().import(quadStream)
+
+          const { entityHtml, entityLabel, entityUrl } = await entityRenderer(
+            request,
+            {
+              dataset,
+              rewriteResponse,
+              replaceIri,
+              entityRoot: rewriteValue ? iri.replace(datasetBaseUrl, iriOrigin(iriUrlString)) : iri,
+            },
+          )
+          const metadata = await metadataProvider(request, { dataset })
+
+          reply.type('text/html').send(await render(entityTemplatePath, {
+            dataset: entityHtml,
+            locals: {},
+            entityLabel,
+            entityUrl,
+            metadata,
+            config,
+          }))
+        } catch (e) {
+          logger.error(e)
+          reply.callNotFound()
+        }
       }
-
-      const dataset = await rdf.dataset().import(quadStream)
-
-      const { entityHtml, entityLabel, entityUrl } = await entityRenderer(
-        req,
-        res,
-        {
-          dataset,
-          rewriteResponse,
-          replaceIri,
-          entityRoot: rewriteValue ? iri.replace(datasetBaseUrl, iriOrigin(iriUrlString)) : iri,
-        },
-      )
-      const metadata = await metadataProvider(req, { dataset })
-
-      res.setHeader('Content-Type', 'text/html')
-      res.send(await render(entityTemplatePath, {
-        dataset: entityHtml,
-        locals: res.locals,
-        entityLabel,
-        entityUrl,
-        metadata,
-        config,
-      }))
-    } catch (e) {
-      logger.error(e)
-      return next()
-    }
+      return handler
+    },
   }
 }
 
--- packages/entity-renderer/renderer/entity.js
+++ packages/entity-renderer/renderer/entity.js
@@ -35,7 +35,8 @@ const toBoolean = (val) => {
  * Render HTML.
  */
 const createEntityRenderer = ({ options = {}, logger, query }) => {
-  return async (req, res, { dataset, rewriteResponse, replaceIri, entityRoot }) => {
+  return async (req, { dataset, rewriteResponse, replaceIri, entityRoot }) => {
+    const currentLanguage = req.cookies.i18n
     const rendererConfig = { ...DEFAULTS, ...options }
 
     // Honor parameters in the request
@@ -71,8 +72,8 @@ const createEntityRenderer = ({ options = {}, logger, query }) => {
     }
 
     rendererConfig.highlightLanguage =
-      req.query.highlightLanguage ??
-      res.locals.currentLanguage ??
+      req.query.highlightLanguage ||
+      currentLanguage ||
       rendererConfig.preferredLanguages[0]
 
     if (req.query.compactMode !== undefined) {
