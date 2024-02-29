b38cbc55812e7df1e0ec14f13351703ab7ccf997
Ludovic Muller <ludovicmuller1@gmail.com>
2024-02-29 12:25:16 +0100

    sparql-proxy: rework the plugin

--- packages/sparql-proxy/index.js
+++ packages/sparql-proxy/index.js
@@ -1,42 +1,106 @@
-import sparqlProxy from '@zazuko/sparql-proxy'
-import { ProxyAgent } from 'proxy-agent'
+// @ts-check
 
-const factory = (trifid) => {
-  const { config } = trifid
+const defaultConfiguration = {
+  endpointUrl: '',
+}
 
-  const {
-    endpointUrl: _e, // ignore this field
-    authentication: _a, // ignore this field
-    enableProxy, // enable/disable the support for `HTTP_PROXY`, `HTTPS_PROXY` and `NO_PROXY` environment variables
-    sparqlEndpoint, // get the configuration about the endpoint
-    ...proxyConfig // rest of the configuration
-  } = config
+/** @type {import('../core/types/index.js').TrifidMiddleware} */
+const factory = async (trifid) => {
+  const { logger, config } = trifid
 
-  if (sparqlEndpoint) {
-    if (Object.hasOwnProperty.call(sparqlEndpoint, 'url')) {
-      proxyConfig.endpointUrl = sparqlEndpoint.url
-    }
+  const options = { ...defaultConfiguration, ...config }
+  if (!options.endpointUrl) {
+    throw Error('Missing endpointUrl parameter')
+  }
 
-    const hasProperties =
-      Object.hasOwnProperty.call(sparqlEndpoint, 'username') &&
-      Object.hasOwnProperty.call(sparqlEndpoint, 'password')
-    if (
-      hasProperties &&
-      sparqlEndpoint.username !== '' &&
-      sparqlEndpoint.password !== ''
-    ) {
-      proxyConfig.authentication = {
-        user: sparqlEndpoint.username,
-        password: sparqlEndpoint.password,
+  return {
+    defaultConfiguration: async () => {
+      return {
+        methods: ['GET', 'POST'],
+        paths: [
+          '/query',
+          '/query/',
+        ],
       }
-    }
-  }
+    },
+    routeHandler: async () => {
+      /**
+       * Query string type.
+       *
+       * @typedef {Object} QueryString
+       * @property {string} [query] The SPARQL query.
+       */
 
-  if (enableProxy && enableProxy !== 'false') {
-    proxyConfig.agent = new ProxyAgent()
-  }
+      /**
+       * Request body type.
+       * @typedef {Object} RequestBody
+       * @property {string} [query] The SPARQL query.
+       */
 
-  return sparqlProxy(proxyConfig)
+      /**
+       * Route handler.
+       * @param {import('fastify').FastifyRequest<{ Querystring: QueryString, Body: RequestBody | string }>} request Request.
+       * @param {import('fastify').FastifyReply} reply Reply.
+       */
+      const handler = async (request, reply) => {
+        const fullUrl = `${request.protocol}://${request.hostname}${request.raw.url}`
+        const fullUrlObject = new URL(fullUrl)
+        const fullUrlPathname = fullUrlObject.pathname
+
+        // Enforce non-trailing slash
+        if (fullUrlPathname.slice(-1) === '/') {
+          return reply.redirect(`${fullUrlPathname.slice(0, -1)}`)
+        }
+
+        let query = ''
+        switch (request.method) {
+          case 'GET':
+            query = request.query.query
+            break
+          case 'POST':
+            if (typeof request.body === 'string') {
+              query = request.body
+            }
+
+            if (typeof request.body !== 'string' && request.body.query) {
+              query = request.body.query
+            }
+
+            if (typeof query !== 'string') {
+              query = JSON.stringify(query)
+            }
+
+            break
+          default:
+            return reply.code(405).send('Method Not Allowed')
+        }
+
+        logger.debug('Got a request to the sparql proxy')
+
+        logger.debug(`Received query: ${query}`)
+
+        try {
+          const acceptHeader = request.headers.accept || 'application/sparql-results+json'
+          const response = await fetch(options.endpointUrl, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              Accept: acceptHeader,
+            },
+            body: new URLSearchParams({ query }),
+          })
+
+          const contentType = response.headers.get('content-type')
+          return reply.status(response.status).header('content-type', contentType).send(response.body)
+        } catch (error) {
+          logger.error('Error while querying the endpoint')
+          logger.error(error)
+          return reply.code(500).send('Error while querying the endpoint')
+        }
+      }
+      return handler
+    },
+  }
 }
 
 export default factory
