23d6f838555f15046cfac59a686bc2062219917c
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-04-10 21:14:32 +0200

    fix: wrong namespace
    Co-authored-by: Ludovic Muller <ludovicm67@users.noreply.github.com>

--- packages/iiif/src/ns.js
+++ packages/iiif/src/ns.js
@@ -3,7 +3,7 @@ import rdf from '@zazuko/env'
 const ns = {
   iiifImage: rdf.namespace('http://iiif.io/api/image/3#'),
   iiifPrezi: rdf.namespace('http://iiif.io/api/presentation/3#'),
-  dctypes: rdf.ns.dctypes,
+  dctypes: rdf.ns.dcmitype,
   exif: rdf.ns.exif,
   dcterms: rdf.ns.dcterms,
   schema: rdf.ns.schema,
