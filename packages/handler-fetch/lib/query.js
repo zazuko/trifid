4b7a59b1d2b8a673771f8015c928695a29ec9a32
Ludovic Muller <ludovicmuller1@gmail.com>
2024-06-05 14:16:12 +0200

    handler-fetch: return as ttl for now

--- packages/handler-fetch/lib/query.js
+++ packages/handler-fetch/lib/query.js
@@ -1,154 +1,47 @@
 // @ts-check
 
 /**
- * Convert Oxigraph termType to SPARQL termType.
- *
- * @param {string} termType Oxigraph termType value
- * @returns {string} SPARQL termType value
- */
-export const convertTermType = (termType) => {
-  switch (termType) {
-    case 'Literal':
-      return 'literal'
-    case 'BlankNode':
-      return 'bnode'
-    case 'NamedNode':
-      return 'uri'
-    default:
-      return 'literal'
-  }
-}
-
-/**
- * Handle Oxigraph query results.
- *
- * @param {ReturnType<import('oxigraph').Store['query']>} results
- * @param {boolean} isConstructQuery
- * @returns {Promise<{
- *   raw: string | Record<string, any> | string[];
- *   response: string;
- *   contentType: 'application/sparql-results+json' | 'application/n-triples';
- *   type: 'ASK' | 'SELECT' | 'CONSTRUCT';
- * }>} SPARQL response.
- */
-export const handleOxigraphResult = async (results, isConstructQuery = false) => {
-  let sparqlResponse = {}
-
-  // Handle ASK queries
-  if (typeof results === 'boolean') {
-    sparqlResponse = {
-      head: {},
-      boolean: results,
-    }
-    return {
-      raw: sparqlResponse,
-      response: JSON.stringify(sparqlResponse, null, 2),
-      contentType: 'application/sparql-results+json',
-      type: 'ASK',
-    }
-  }
-
-  // Handle empty results
-  if (!results || !Array.isArray(results) || results.length === 0) {
-    if (isConstructQuery) {
-      return {
-        raw: '',
-        response: '',
-        contentType: 'application/n-triples',
-        type: 'CONSTRUCT',
-      }
-    }
-
-    sparqlResponse = {
-      head: {
-        vars: [],
-      },
-      results: {
-        bindings: [],
-      },
-    }
-    return {
-      raw: sparqlResponse,
-      response: JSON.stringify(sparqlResponse, null, 2),
-      contentType: 'application/sparql-results+json',
-      type: 'SELECT',
-    }
-  }
-
-  const headVariables = new Set()
-  const bindings = []
-  let isOtherThanMap = false
-
-  // Loop over each result, and build bindings and variables
-  // We assume that all results are `Map` objects for SELECT queries
-  // If we get something else than `Map` objects, we assume it's a CONSTRUCT query
-  for (const result of results) {
-    if (result instanceof Map) {
-      const binding = {}
-      for (const [key, value] of result) {
-        headVariables.add(key)
-        binding[key] = {
-          type: convertTermType(value.termType),
-          value: value.value,
-        }
-        if (value.language) {
-          binding[key]['xml:lang'] = value.language
-        }
-        if (value.datatype) {
-          binding[key].datatype = value.datatype.value
-        }
-      }
-      bindings.push(binding)
-    } else {
-      isOtherThanMap = true
-      break
-    }
-  }
-
-  // We got something else than `Map` objects, so we assume it's a CONSTRUCT query
-  if (isOtherThanMap) {
-    const quads = results.map((quad) => quad.toString())
-    const quadsOutput = `${quads.join(' . \n')} .`
-    return {
-      raw: quads,
-      response: quadsOutput,
-      contentType: 'application/n-triples',
-      type: 'CONSTRUCT',
-    }
-  }
-
-  // Build the SPARQL response for the SELECT query
-  sparqlResponse = {
-    head: {
-      vars: Array.from(headVariables),
-    },
-    results: {
-      bindings,
-    },
-  }
-
-  return {
-    raw: sparqlResponse,
-    response: JSON.stringify(sparqlResponse, null, 2),
-    contentType: 'application/sparql-results+json',
-    type: 'SELECT',
-  }
-}
-
-/**
  * Perform a SPARQL query using Oxigraph.
  *
  * @param {import('oxigraph').Store} store Oxigraph store
  * @param {string} query The query to perform
  * @returns {Promise<{
- *   raw: string | Record<string, any> | string[];
  *   response: string;
- *   contentType: 'application/sparql-results+json' | 'application/n-triples';
- *   type: 'ASK' | 'SELECT' | 'CONSTRUCT';
+ *   contentType: 'application/sparql-results+json' | 'text/turtle' | 'text/plain' | string & Record<never, never>;
  * }>} SPARQL response.
  */
 export const performOxigraphQuery = async (store, query) => {
-  const results = store.query(query)
-  const isConstructQuery = query.toUpperCase().includes('CONSTRUCT')
-  return await handleOxigraphResult(results, isConstructQuery)
+  let contentType = 'text/plain'
+  let results = 'Something went wrong.'
+
+  const queryUpper = query.toUpperCase()
+  const isConstructQuery = queryUpper.includes('CONSTRUCT') || queryUpper.includes('DESCRIBE')
+
+  try {
+    if (isConstructQuery) {
+      contentType = 'text/turtle'
+      results = store.query(query, {
+        use_default_graph_as_union: true,
+        results_format: 'text/turtle',
+      })
+    } else {
+      contentType = 'application/sparql-results+json'
+      results = store.query(query, {
+        use_default_graph_as_union: true,
+        results_format: 'json',
+      })
+    }
+  } catch (error) {
+    contentType = 'text/plain'
+    if (error instanceof Error) {
+      results = error.message
+    } else {
+      results = 'An error occurred while executing the query.'
+    }
+  }
+
+  return {
+    response: results,
+    contentType,
+  }
 }
