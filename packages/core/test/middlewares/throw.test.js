27633ff8477afc315cc8d924100b51a2b5bf6f03
Ludovic Muller <ludovicmuller1@gmail.com>
2024-07-16 09:34:12 +0200

    test(core): add tests for the throw plugin

---
+++ packages/core/test/middlewares/throw.test.js
@@ -0,0 +1,49 @@
+// @ts-check
+
+import { strictEqual } from 'node:assert'
+
+import { describe, it } from 'mocha'
+import trifidCore, { getListenerURL } from '../../index.js'
+
+import throwPlugin from '../../plugins/throw.js'
+
+const createTrifidInstance = async (config) => {
+  return await trifidCore({
+    server: {
+      listener: {
+        port: 4242,
+      },
+      logLevel: 'warn',
+    },
+  }, {
+    throw: {
+      module: throwPlugin,
+      paths: ['/throw'],
+      config,
+    },
+  })
+}
+
+describe('throw plugin', () => {
+  it('should throw using the default configuration', async () => {
+    const trifidInstance = await createTrifidInstance({})
+    const trifidListener = await trifidInstance.start()
+    const pluginUrl = `${getListenerURL(trifidListener)}/throw`
+    const response = await fetch(pluginUrl)
+    await trifidListener.close()
+
+    strictEqual(response.status, 500)
+  })
+
+  it('should allow a custom message', async () => {
+    const trifidInstance = await createTrifidInstance({
+      message: 'This is a custom error message',
+    })
+    const trifidListener = await trifidInstance.start()
+    const pluginUrl = `${getListenerURL(trifidListener)}/throw`
+    const response = await fetch(pluginUrl)
+    await trifidListener.close()
+
+    strictEqual(response.status, 500)
+  })
+})
